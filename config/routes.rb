80b5fb542af98a925ed1c49f23a0ab81ee8a5a9b
Atul Tiwari <atul22king@gmail.com>
2022-02-19 17:08:57 +0530

    added the i18n gem to manages the i18n flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
- root 'sessions#new'
- 
- resources :sessions
- resources :homes
- resources :books
- resources :booths
- resources :operations
- resources :categories
+  root 'sessions#new'
+  scope "(:locale)", locale: /en|ar/ do 
+    resources :sessions
+    resources :homes
+    resources :books
+    resources :booths
+    resources :operations
+    resources :categories
+  end
 end
