5f1d73b0e114fd959a5540b1140ba07e8360a5c9
Atul Tiwari <atul22king@gmail.com>
2022-02-22 09:47:39 +0530

    make form dynamic for the booth

--- app/controllers/booths_controller.rb
+++ app/controllers/booths_controller.rb
@@ -1,5 +1,26 @@
 class BoothsController < ApplicationController
   before_action :logged_in?
   def index
+    @booth = Booth.new
+    @booths = Booth.all
+  end
+
+  def create
+    @booth = Booth.new(booth_params)
+    if @booth.save
+      redirect_to booths_path, notice: "Booth Create Successfully!"
+    else
+      redirect_to booths_path, alert: @booth.errors.full_messages
+    end
+  end
+
+  private
+
+  def booth_params
+    coordinate = params[:booth][:coordinate]
+    latitude = coordinate.split(",").first
+    longitude = coordinate.split(",").second
+    params[:booth].merge!({latitude: latitude, longitude: longitude})
+    params.require(:booth).permit(:name, :city, :address, :phone_number, :latitude, :longitude)
   end
 end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :logged_in?
   def index
+    @categories = Category.all
   end
 end
