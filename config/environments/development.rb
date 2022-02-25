33fdaffdec4a0c8b1c6da142c12a845e9b4a6b5a
Atul Tiwari <atul22king@gmail.com>
2022-02-25 16:51:24 +0530

    send mail through google

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -77,11 +77,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true #Needed if this is dev env. file
   config.action_mailer.smtp_settings = {
-    address:                 'smtp.sendgrid.net',
+    address:                 'smtp.gmail.com',
     port:                     587,
     domain:                   'localhost',
-    user_name:                 'khaledmoc12',
-    password:                  'Print@moc123',
+    user_name:                 'developmenttest127@gmail.com',
+    password:                  'dev@123456789',
     authentication:           'plain',
     enable_starttls_auto:    true
   }
