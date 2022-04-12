6ead647bbe4d5d03a07d4808bff72500240c6908
Atul Tiwari <atul22king@gmail.com>
2022-04-12 15:25:53 +0530

    integrate the arabic html

--- app/controllers/web/web_application_controller.rb
+++ app/controllers/web/web_application_controller.rb
@@ -1,3 +1,9 @@
 class Web::WebApplicationController < ActionController::Base
   layout 'web_layout'
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = :en
+    I18n.locale = params[:locale]
+  end
 end
