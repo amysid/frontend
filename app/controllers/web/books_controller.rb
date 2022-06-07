9b9476276f3c3694eea20f4a84daa6e86d9ccf76
Atul Tiwari <atul22king@gmail.com>
2022-06-07 23:42:59 +0530

    show all the book in booth

--- app/controllers/web/books_controller.rb
+++ app/controllers/web/books_controller.rb
@@ -3,20 +3,18 @@ class Web::BooksController < Web::WebApplicationController
   before_action :fetch_categories
 
   def index
-    language = params[:locale] == "ar" ? "Arabic" : "English" 
     book_ids = @booth.books.pluck(:book_id)
-    @books = Book.includes(:book_files).where(id: book_ids, status: "Published", language: language).order('created_at desc')
+    @books = Book.includes(:book_files).where(id: book_ids, status: "Published").order('created_at desc')
 
     book_ids_from_operation = Operation.where(booth_id: @booth.id).pluck(:book_id)
     @trending_books = @books.where(id: book_ids_from_operation)
     @trending_books = @books if @trending_books.blank?
   end
 
-  def search
-    language = params[:locale] == "ar" ? "Arabic" : "English" 
+  def search 
     if params[:book].present?
       book_ids = @booth.books.pluck(:book_id)
-      @books = Book.includes(:book_files).where(id: book_ids, language: language, status: "Published").order('created_at desc')
+      @books = Book.includes(:book_files).where(id: book_ids, status: "Published").order('created_at desc')
       @books = @books.includes(:book_files).where("books.title ILIKE ? OR books.author_name ILIKE ?  OR books.body ILIKE ?", "%#{params[:book]}%", "%#{params[:book]}%", "%#{params[:book]}%" ).order('created_at desc')
     end
     if params[:type] == "all"
@@ -29,10 +27,9 @@ class Web::BooksController < Web::WebApplicationController
   end
 
   def category_search
-    language = params[:locale] == "ar" ? "Arabic" : "English" 
     if params[:category_id].present?
       book_ids = @booth.books.pluck(:book_id)
-      @books = Book.includes(:book_files).where(id: book_ids, language: language, status: "Published")
+      @books = Book.includes(:book_files).where(id: book_ids, status: "Published")
       book_ids = @categories.pluck(:book_id)
       @books = @books.where(id: book_ids)
       @total_books = @books.count || 0
