619d8418ad3517ef6b75afc6459897a2884f2fe8
Atul Tiwari <atul.tiwari@coralmango.com>
2022-07-03 17:38:47 +0530

    API integration for mobile page

--- app/controllers/web/operations_controller.rb
+++ app/controllers/web/operations_controller.rb
@@ -1,31 +1,26 @@
 class Web::OperationsController < Web::WebApplicationController
-  before_action :set_operation
   layout false
 
   def media_files
-    @book = @operation.book
-    language = params[:locale] == "en" ? "English" : "Arabic"
-    @operation.update(language: language)
-    @book.update(last_listening_at: Time.now)
-  end
-
-  def update_listen_count
-    current_listen_time = params[:current_time].to_f
-    total_time = params[:file_duration].to_f
-    listen_time = current_listen_time.to_f
-    @operation.update(listening_time: Time.now, listening_status: listen_time)
-    render json: {message: "successfully save count"}
+    url = "#{ENV["API_BASE_URL"]}/web_api/operations/#{params[:id]}/media_files"
+    headers = {"Content-Type": "application/json"}
+    response = HTTParty.get(url, headers: headers)
+    response_body = JSON.parse(response.body) if response.body.present?
+    if response_body.present? &&  response_body.dig("book").present? && response_body.dig("operation").present?
+      @book = response_body["book"]["data"]["attributes"]
+      @operation = response_body["operation"]["data"]["attributes"]
+    end
   end
 
   def save_feedback
-    @operation.update(rating: params[:feedback][:rating], note: params[:feedback][:note])
-    # redirect_to media_files_web_operation_path(id: @operation.number), notice: t("feedback save successfully")
-    render json: {message: "data save successfully"}
+    byebug
+    url = "#{ENV["API_BASE_URL"]}/web_api/operations/#{params[:id]}/save_feedback"
+    headers = {"Content-Type": "application/json", body: params.as_json}
+    response = HTTParty.put(url, headers: headers)
+    response_body = JSON.parse(response.body) if response.body.present?
+    if response_body.present? &&  response_body.dig("message").present?
+      render json: {message: "data save successfully"}
+    end
   end
 
-  private
-
-  def set_operation
-    @operation = Operation.find_by(number: params[:id])
-  end
 end
