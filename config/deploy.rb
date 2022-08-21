7edac464969b1618d1cfb7c1e09b36dc93e1862a
amysid <ammadsid87@gmail.com>
2022-08-21 16:20:48 +0530

    cap conf

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock "~> 3.17.1"
 
 set :repo_url,        'https://github.com/amysid/audiolibrary-frontend.git'
 set :application,     'audiolibrary-frontend'
-set :user,            'root'
+set :user,            'ec2-user'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
@@ -21,7 +21,12 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+# set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :ssh_options, {
+  forward_agent: true,
+  auth_methods: %w[publickey],
+  keys: %w[/home/ammad/Downloads/rhel.pem]
+}
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, false  # Change to false when not using ActiveRecord
@@ -43,7 +48,7 @@ set :keep_releases, 3
 ## Linked Files & Directories (Default None):
 
 # set :linked_files, %w{config/database.yml}
-# set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
